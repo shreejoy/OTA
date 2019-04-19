b1bccb69f615604aeb06b6e918325593063bbcaf
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-04-19 09:38:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ for i in data:
     if ': \n' in i or '()' in i:
         data.remove(i)
 # create the message
-message = ''.join(data)
+telegram_message = ''.join(data)
 
 
 photo = info[0]['image']
