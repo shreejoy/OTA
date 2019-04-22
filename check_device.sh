3e9e48b08d88dd4adb98f9326ca5c4ca506e592e
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-04-22 08:18:36 +0530

    Create check_device.sh

---
+++ check_device.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+      
+      export ID=$TRAVIS_COMMIT
+	  git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT > commit.txt
+    echo "$(<commit .txt )"
+      cat commit.txt | cut -d "/" -f1
+      
+for i in $(jq -r ".[] | .codename" devices.json)
+  do
+      if grep $i commit.txt; then
+      echo -e " ";
+      echo -e "There is a new update for $i ";
+      echo -e " ";
+      echo -e "Getting build information from remote source !!";
+      echo -e " ";
+else
+      echo -e "No new update for $i"
+      fi
+done	
