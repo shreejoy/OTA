b46ce44e9e252716f9f6062c191221d23ce8cc7c
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-05-01 11:01:23 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,7 +3,8 @@
 export ID=fc72922bcf7d03a2c21c2da18a6db99ddbf1279f
 export bottoken=874915983:AAHXpzpNAQq2RJDToG4ASWts7_bx119EuJY
 export COMMIT_ID=$(git log --format="%H" -n 1)
-export CODENAME=$(git diff-tree --no-commit-id --name-only -r $ID | cut -d "/" -f1)
+# export CODENAME=$(git diff-tree --no-commit-id --name-only -r $ID | cut -d "/" -f1)
+export CODENAME=santoni
 echo "There is a Update for $CODENAME";
 DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
 git clone  https://gitlab.com/pshreejoy15/ota.git
