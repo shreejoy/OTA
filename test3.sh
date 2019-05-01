7a4d94403ad3e983b055aa364f90aa69b656731f
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-05-01 10:35:09 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env sh
-MERGE_PREFIX="Merge pull request"
+#! /bin/sh
+      
+export ID=fc72922bcf7d03a2c21c2da18a6db99ddbf1279f
+export bottoken=874915983:AAHXpzpNAQq2RJDToG4ASWts7_bx119EuJY
+export COMMIT_ID=$(git log --format="%H" -n 1)
+export CODENAME=$(git diff-tree --no-commit-id --name-only -r $ID | cut -d "/" -f1)
+echo "There is a Update for $CODENAME";
+DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
+git clone  https://gitlab.com/pshreejoy15/ota.git
+rm -rf ota/pixys.json
+echo "$DEVICE" >> ota/pixys.json
+cd ota
+wget https://gitlab.com/pshreejoy15/ota/raw/master/jsonFormatter.py 
+python3 jsonFormatter.py
+cd ..
+python3 new.py
 
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    TRAVIS_COMMIT_RANGE="$(git describe --abbrev=0 --tags $TRAVIS_TAG^)..$TRAVIS_TAG"
-fi
 
-GIT_COMMIT_LOG="$(git log --format='%s (by %cn)' $TRAVIS_COMMIT_RANGE)"
 
-echo " <b>Changelog for TelegramXMods-${TRAVIS_BUILD_NUMBER}</b>${NEWLINE}"
-echo << "$TRAVIS_TAG"
-printf '%s\n' "$GIT_COMMIT_LOG" | while IFS= read -r line
-do
-  echo "- ${line}"
-done
