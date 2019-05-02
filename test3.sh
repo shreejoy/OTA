83067546350976287d1bfaaeb736a58237321568
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-05-02 11:05:02 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,6 +3,7 @@
 # export ID=fc72922bcf7d03a2c21c2da18a6db99ddbf1279f
 export COMMIT_ID=$(git log --format="%H" -n 1)
 export CODENAME=$(git diff-tree --no-commit-id --name-only -r $COMMIT_ID | cut -d "/" -f1)
+export DTIME=$(date)
 # export CODENAME=santoni
 echo "There is a Update for $CODENAME";
 DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
