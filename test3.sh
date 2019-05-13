b326d53c452d9038dff18fdd010a6431f4362089
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-05-13 22:14:00 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -5,21 +5,32 @@ export COMMIT_ID=$(git log --format="%H" -n 1)
 export CODENAME=$(git diff-tree --no-commit-id --name-only -r $COMMIT_ID | cut -d "/" -f1)
 export DTIME=$(date)
 # export CODENAME=santoni
-echo "There is a Update for $CODENAME";
-DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
-git clone  git@gitlab.com:pshreejoy15/rom_ota.git
-rm -rf rom_ota/pixys.json
-echo "$DEVICE" >> rom_ota/pixys.json
-cd rom_ota
-wget https://gitlab.com/pshreejoy15/ota/raw/master/jsonFormatter.py 
-python3 jsonFormatter.py
-git config --global user.name "Shreejoy Dash"
-git config --global user.email "pshreejoy15@gmail.com"
-git add pixys.json 
-git commit -m "$(date) $CODENAME update"
-git push
-cd ..
-python3 new.py
+mkdir check
+cd check
+   wget -q https://raw.githubusercontent.com/PixysOS-Devices/official_devices/master/$CODENAME/build.json
+   URL=$(jq -r '.response.url' build.json)
+   if wget -q "$URL"; then
+      echo "There is a Update for $CODENAME";
+      if CODENAME==devices.json; then
+         echo -e "There is some changes to devices.json, So nothing to post"
+         exit
+      else
+         DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
+         git clone  git@gitlab.com:pshreejoy15/rom_ota.git
+         rm -rf rom_ota/pixys.json
+         echo "$DEVICE" >> rom_ota/pixys.json
+         echo "$DTIME -----> $CODENAME" >> rom_ota/Update_list.txt
+         cd rom_ota
+            wget https://gitlab.com/pshreejoy15/ota/raw/master/jsonFormatter.py 
+            python3 jsonFormatter.py
+            git config --global user.name "Shreejoy Dash"
+            git config --global user.email "pshreejoy15@gmail.com"
+            git add pixys.json Update_list.txt
+            git commit -m "$(date) $CODENAME update"
+            git push
+         cd ..
+         python3 tg_post.py
+fi
 
 
 
