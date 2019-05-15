0dcbabead19a6f174061d820f089802ef8370d56
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-05-15 16:22:23 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,36 +3,45 @@
 # export ID=fc72922bcf7d03a2c21c2da18a6db99ddbf1279f
 export COMMIT_ID=$(git log --format="%H" -n 1)
 export CODENAME=$(git diff-tree --no-commit-id --name-only -r $COMMIT_ID | cut -d "/" -f1)
+if [ -z "$CODENAME" ]; then 
+  echo "This is a merge commit" 
+  export CODENAME=$(git log -m -1 --name-only --pretty="format:" $COMMIT_ID | cut -d "/" -f1)
+else 
+  echo "This is not a merge commit";
+fi
 export DTIME=$(date)
 # export CODENAME=santoni
 mkdir check
 cd check
    wget -q https://raw.githubusercontent.com/PixysOS-Devices/official_devices/master/$CODENAME/build.json
    URL=$(jq -r '.response.url' build.json)
-cd ..   
+   FNAME=$(jq -r '.response.filename' build.json)
+cd ..
+if CODENAME==devices.json; then
+   echo -e "There is some changes to devices.json, So nothing to post"
+   exit
+else
    if wget -q "$URL"; then
-      echo "There is a Update for $CODENAME";
-      if CODENAME==devices.json; then
-         echo -e "There is some changes to devices.json, So nothing to post"
-         exit
-      else
-         DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
-         git clone  git@gitlab.com:pshreejoy15/rom_ota.git
-         rm -rf rom_ota/pixys.json
-         echo "$DEVICE" >> rom_ota/pixys.json
-         echo "$DTIME -----> $CODENAME" >> rom_ota/Update_list.txt
-         cd rom_ota
-            wget https://gitlab.com/pshreejoy15/ota/raw/master/jsonFormatter.py 
-            python3 jsonFormatter.py
-            git config --global user.name "Shreejoy Dash"
-            git config --global user.email "pshreejoy15@gmail.com"
-            git add pixys.json Update_list.txt
-            git commit -m "$(date) $CODENAME update"
-            git push
-         cd ..
-         python3 tg_post.py
-      fi
-    else
+      echo -e "Link is valid for OTA"
+      echo "[RELEASING BUILD] $FNAME"
+      DEVICE="[$(cat devices.json | jq -c --arg CODENAME "$CODENAME" '.[] | select(.codename==$CODENAME)')]"
+      git clone  git@gitlab.com:pshreejoy15/rom_ota.git
+      rm -rf rom_ota/pixys.json
+      echo "$DEVICE" >> rom_ota/pixys.json
+      echo "$DTIME -----> $CODENAME" >> rom_ota/Update_list.txt
+      cd rom_ota
+         wget https://gitlab.com/pshreejoy15/ota/raw/master/jsonFormatter.py 
+         python3 jsonFormatter.py
+         git config --global user.name "Shreejoy Dash"
+         git config --global user.email "pshreejoy15@gmail.com"
+         git add pixys.json Update_list.txt
+         git commit -m "$DTIME $CODENAME update"
+         git push
+      cd ..
+      python3 tg_post.py
+   else
+   fi
+fi
     
 
 
