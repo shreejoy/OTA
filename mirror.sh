28d89413ec82a44fd6bc7cb68a9ef69a2dcd3717
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-06-30 19:15:30 +0530

    Create mirror.sh

---
+++ mirror.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /home/rof/src/github.com/shreejoy/test
+wget -O /home/rof/src/github.com/shreejoy/test/devices.json https://raw.githubusercontent.com/shreejoy/test/master/devices.json?token=AJKZ7EFWUY4EWYVZWYFJLNC5EH2KM
+LINK=$(jq -r '.url' devices.json)
+FILE_NAME=$(jq -r '.fname' devices.json)
+wget $LINK
+echo "Mirroring $FILE_NAME"
+scp $FILE_NAME pshreejoy15@frs.sourceforge.net:/home/frs/p/shreejoy
