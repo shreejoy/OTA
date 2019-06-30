8d4ddbe1d5bb98a4be5a35de51d0bed17855fe1d
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-06-30 19:18:42 +0530

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/rof/src/github.com/shreejoy/test
-wget -O /home/rof/src/github.com/shreejoy/test/devices.json https://raw.githubusercontent.com/shreejoy/test/master/devices.json?token=AJKZ7EFWUY4EWYVZWYFJLNC5EH2KM
+#wget -O /home/rof/src/github.com/shreejoy/test/devices.json https://raw.githubusercontent.com/shreejoy/test/master/devices.json?token=AJKZ7EFWUY4EWYVZWYFJLNC5EH2KM
 LINK=$(jq -r '.url' devices.json)
 FILE_NAME=$(jq -r '.fname' devices.json)
 wget $LINK
