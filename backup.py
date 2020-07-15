314a05e6a11da9b7fdb2009165db1b6b9ea9c393
Shreejoy Dash <pshreejoy15@gmail.com>
2020-07-15 12:58:24 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,14 +1,20 @@
 from github import Github
-import pymongo, json
+import pymongo, json, requests
 
 EDITION = 'NORMAL'
 FILENAME = 'build.json'
 REPO = 'PixysOS-Devices/official_devices'
 BRANCH = 'master'
 VERSION_CODE = 'ten'
+MONGO_AUTH_URI = ''
+MONGO_INIT = pymongo.MongoClient(MONGO_AUTH_URI)
+DB = MONGO_INIT['PixysOS']
+COLLECTION = DB['builds']
+
+auth = Github('shreejoy')
 
 try:
-    DEVICES_JSON = json.loads(auth.get_repo('PixysOS-Devices/official_devices').get_contents(path="devices.json", ref="ten").decoded_content)
+    DEVICES_JSON = json.loads(auth.get_repo('PixysOS/official_devices').get_contents(path="devices.json", ref="ten").decoded_content)
 except:
     print('Error loading json')
     
@@ -17,23 +23,25 @@ for DEVICE_JSON in DEVICES_JSON:
     codename = DEVICE_JSON['codename']
     codenames.append(codename)
  
-official_builds = auth.get_repo(REPO).get_branch(BRANCH)
+official_builds = auth.get_repo(REPO)
 for codename in codenames:
     path = codename + '/' + FILENAME
-    content official_builds.get_content(path=path).decoded_content
+    #content  = official_builds.get_contents(path=path, ref="ten").decoded_content
     try:
+        content  = official_builds.get_contents(path=path, ref="ten").decoded_content
         content = json.loads(content)
-    break:
+    except:
         print(codename + ' build json is wrong')
-        break
+        continue
     build = {}
-    response = content['response'] 
+    response = content['response'][0]
     build['filename'] = response['filename']
     build['md5'] = response['id']
     build['size'] = response['size']
-    build['url'] = response['url']
-    build['version'] = 
+    build['version'] = response['version']
+    build['device'] = codename
     build['edition'] = EDITION
     build['version_code'] = VERSION_CODE
-    
-    
+    build['url'] = requests.get('https://get.pixys.tools/utils/crypto/' + build['device'] + '/' + build['version_code'] + '/' + build['filename']).text
+    print(build)
+    COLLECTION.insert_one(build)
